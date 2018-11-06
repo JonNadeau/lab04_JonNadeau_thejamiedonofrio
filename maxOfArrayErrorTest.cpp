24441f9c80a8d114158b52aabf11404a7e576fa4
Jon Nadeau <jonathannadeau@cs.ucsb.edu>
2018-11-06 15:57:22 -0800

    fthis

--- maxOfArrayErrorTest.cpp
+++ maxOfArrayErrorTest.cpp
@@ -1,17 +1,17 @@
 #include "arrayFuncs.h"
 #include "tddFuncs.h"
 
-#include <cstdlib>
-#include <iostream>
+//#include <cstdlib>
+//#include <iostream>
 
 int main() {
 
   int empty[] = {};
   // expect this function to result in message to cerr and exit(1);
-cerr<<"ERROR: maxOfArray called with size < 1";
-exit(1);
+//cerr<<"ERROR: maxOfArray called with size < 1";
+//exit(1);
   
-  assertEquals(0,
+  assertEquals("ERROR: maxOfArray called with size < 1",
 	       maxOfArray(empty,0), 
 	       "maxOfArray(empty,0)" ); 
   return -42;
