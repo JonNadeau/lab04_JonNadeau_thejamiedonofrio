44a9f593f0f5f6f4b2ce3c544f280f01a796c020
Jon Nadeau <jonathannadeau@cs.ucsb.edu>
2018-11-06 15:00:01 -0800

    sauce

--- maxOfArray.cpp
+++ maxOfArray.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 
 int maxOfArray(int a[], int size) {
-	int max;
+
 	if(size==0)
 		return 0;
-	for(int i=0; i<size; i++){
+	int max=a[0];
+	for(int i=1; i<size; i++){
 		if(a[i]>max)
 			max=a[i];
 	}
