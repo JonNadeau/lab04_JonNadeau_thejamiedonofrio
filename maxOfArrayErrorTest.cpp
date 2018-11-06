b3409c3a2614ad2172fe83b9160b3df62addad23
Jon Nadeau <jonathannadeau@cs.ucsb.edu>
2018-11-06 15:45:23 -0800

    lol

--- maxOfArrayErrorTest.cpp
+++ maxOfArrayErrorTest.cpp
@@ -5,10 +5,9 @@ int main() {
 
   int empty[] = {};
   // expect this function to result in message to cerr and exit(1);
-  if(sizeof(empty)==0){
-  	cerr<<"ERROR:  maxOfArray called with size < 1";
-	exit(1);
-  }
+cerr<<"ERROR:  maxOfArray called with size < 1";
+exit(1);
+  
   assertEquals(0,
 	       maxOfArray(empty,0), 
 	       "maxOfArray(empty,0)" ); 
